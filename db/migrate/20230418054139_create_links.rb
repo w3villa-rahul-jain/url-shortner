d81d82c812b58d4e48e103a1d361f3b902a5d223
Rahul Jain <rahul.jain@w3villa.com>
2023-04-21 11:59:51 +0530

    add logout button on main page

--- db/migrate/20230418054139_create_links.rb
+++ db/migrate/20230418054139_create_links.rb
@@ -10,22 +10,4 @@ class CreateLinks < ActiveRecord::Migration[6.1]
 end
 
 
-# class AddClickCountToUrls < ActiveRecord::Migration[6.1]
-#   def change
-#     add_column :urls, :click_count, :integer
-#   end
-# end
-
-
-# class CreateUrls < ActiveRecord::Migration[6.1]
-#   def change
-#     create_table :urls do |t|
-#       t.string :original_url
-#       t.string :short_url
-
-#       t.timestamps
-#     end
-#     add_index :urls, :short_url
-#   end
-# end 
 
